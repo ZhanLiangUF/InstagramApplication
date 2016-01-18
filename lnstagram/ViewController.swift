398f1f4eafac1f0fa9b9c8e9e9fded04b4b1fc8d
ZhanLiangUF <zliang@ufl.edu>
2016-01-17 22:29:55 -0500

    Initial Commit

---
+++ lnstagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lnstagram
+//
+//  Created by Abby Juan on 1/17/16.
+//  Copyright © 2016 ZhanLiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
